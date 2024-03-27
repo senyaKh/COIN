ff9d980db4872b9bbc22dea9fc44d0f98c88b252
sisika0 <avatar008009mail.ru@gmail.com>
2024-03-27 21:54:31 +0300

    Улучшение структуры проекта и логики между компонентами

---
+++ src/components/common/Columns.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Coin } from '../../services/types';
+import AddButton from '../ui/AddButton';
+const handleAdd = (coinId: string) => {
+   console.log(`Add coin with ID: ${coinId}`);
+};
+export const columns = [
+ {
+    title: 'Symbol',
+    dataIndex: 'symbol',
+    key: 'symbol',
+ },
+ {
+    title: 'Logo',
+    dataIndex: 'logoUrl',
+    key: 'logoUrl',
+    render: (logoUrl: string) => (
+      <img
+        src={logoUrl}
+        alt='logo'
+        width={30}
+        height={30}
+        style={{ borderRadius: '50%' }}
+        onError={(e) => {
+          e.currentTarget.onerror = null;
+          e.currentTarget.src = '/favicon.ico';
+        }}
+      />
+    ),
+ },
+ {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+ },
+ {
+    title: 'Price (USD)',
+    dataIndex: 'priceUsd',
+    key: 'priceUsd',
+    render: (price: any) => `$${formatPrice(price)}`,
+ },
+ {
+    title: 'Market Cap (USD)',
+    dataIndex: 'marketCapUsd',
+    key: 'marketCapUsd',
+    render: (marketCap: any) => `$${formatPrice(marketCap)}`,
+ },
+ {
+    title: 'Change (24hr)',
+    dataIndex: 'changePercent24Hr',
+    key: 'changePercent24Hr',
+    render: (change: any) => `${formatPercentage(change)}%`,
+ },
+ {
+    title: 'Action',
+    key: 'action',
+    render: (text: string, record: Coin) => (
+      <AddButton coinId={record.id} onAdd={handleAdd} />
+    ),
+ },
+];
+
+function formatPrice(price: number): string {
+ return Number(price).toFixed(2);
+}
+
+function formatPercentage(percentage: number): string {
+ return Number(percentage).toFixed(2);
+}
--- src/components/pages/CoinTable.tsx
+++ src/components/pages/CoinTable.tsx
@@ -3,107 +3,57 @@ import { Table } from 'antd';
 import { fetchCoins } from '../../services/api/coincap';
 import { Coin } from '../../services/types';
 import AddButton from '../ui/AddButton';
+import {columns} from '../common/Columns';
 interface CoinTableProps {}
 
 const CoinTable: React.FC<CoinTableProps> = () => {
-	const [coins, setCoins] = useState<Coin[]>([]);
-	const [loading, setLoading] = useState<boolean>(true);
-	const [currentPage, setCurrentPage] = useState<number>(1);
-	const [totalCoins, setTotalCoins] = useState<number>(0);
+ const [coins, setCoins] = useState<Coin[]>([]);
+ const [loading, setLoading] = useState<boolean>(true);
+ const [currentPage, setCurrentPage] = useState<number>(1);
+ const [totalCoins, setTotalCoins] = useState<number>(0);
 
-	useEffect(() => {
-		const loadCoins = async () => {
-			setLoading(true);
-			const fetchedCoins = await fetchCoins(currentPage);
-			const coinsWithKeys = fetchedCoins.data.map((coin, index) => ({
-				...coin,
-				key: coin.id || index,
-				logoUrl: getLogoUrl(coin.symbol),
-			}));
-			setCoins(coinsWithKeys);
-			setTotalCoins(fetchedCoins.total);
-			setLoading(false);
-		};
+ useEffect(() => {
+    const loadCoins = async () => {
+      setLoading(true);
+      const fetchedCoins = await fetchCoins(currentPage);
+      const coinsWithKeys = fetchedCoins.data.map((coin, index) => ({
+        ...coin,
+        key: coin.id || index,
+        logoUrl: getLogoUrl(coin.symbol),
+      }));
+      setCoins(coinsWithKeys);
+      setTotalCoins(fetchedCoins.total);
+      setLoading(false);
+    };
 
-		loadCoins();
-	}, [currentPage]);
+    loadCoins();
+ }, [currentPage]);
 
-	const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
-		const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
-		if (scrollHeight - scrollTop === clientHeight) {
-			setCurrentPage((prevPage) => prevPage + 1);
-		}
-	};
+ const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
+    if (scrollHeight - scrollTop === clientHeight) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+ };
 
-	const getLogoUrl = (symbol: string) => {
-		return `https://www.cryptocdn.co/icons/colored/${symbol.toLowerCase()}.svg`;
-	};
-	const handleAdd = (coinId: string) => {
-		console.log(`Add coin with ID: ${coinId}`);
-	};
-	return (
-		<div onScroll={handleScroll} style={{ maxHeight: '100vh' }}>
-			<Table
-				columns={[
-					{
-						title: 'Symbol',
-						dataIndex: 'symbol',
-						key: 'symbol',
-					},
-					{
-						title: 'Logo',
-						dataIndex: 'logoUrl',
-						key: 'logoUrl',
-						render: (logoUrl: string) => (
-							<img
-								src={logoUrl}
-								alt='logo'
-								width={30}
-								height={30}
-								style={{ borderRadius: '50%' }}
-								onError={(e) => {
-									e.currentTarget.onerror = null;
-									e.currentTarget.src = '/favicon.ico';
-								}}
-							/>
-						),
-					},
-					{
-						title: 'Name',
-						dataIndex: 'name',
-						key: 'name',
-					},
-					{
-						title: 'Price (USD)',
-						dataIndex: 'priceUsd',
-						key: 'priceUsd',
-						render: (price: any) => `$${Number(price).toFixed(2)}`,
-					},
-					{
-						title: 'Market Cap (USD)',
-						dataIndex: 'marketCapUsd',
-						key: 'marketCapUsd',
-						render: (marketCap: any) => `$${Number(marketCap).toFixed(2)}`,
-					},
-					{
-						title: 'Change (24hr)',
-						dataIndex: 'changePercent24Hr',
-						key: 'changePercent24Hr',
-						render: (change: any) => `${Number(change).toFixed(2)}%`,
-					},
-					{
-						title: 'Action',
-						key: 'action',
-						render: (text: string, record: Coin) => (
-							<AddButton coinId={record.id} onAdd={handleAdd} />
-						),
-					},
-				]}
-				dataSource={coins}
-				loading={loading}
-			/>
-		</div>
-	);
+ const getLogoUrl = (symbol: string) => {
+    return `https://www.cryptocdn.co/icons/colored/${symbol.toLowerCase()}.svg`;
+ };
+ return (
+    <div onScroll={handleScroll} style={{ maxHeight: '100vh' }}>
+      <Table
+        columns={columns}
+        dataSource={coins}
+        loading={loading}
+        pagination={{
+          total: totalCoins,
+          pageSize: 10, 
+          current: currentPage,
+          onChange: (page) => setCurrentPage(page),
+        }}
+      />
+    </div>
+ );
 };
 
 export default CoinTable;
